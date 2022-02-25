8b2df375b33589e745ee3317b65832c521cd56ef
Ati Vassileva <ativassileva@gmail.com>
2022-02-25 12:35:14 +0200

    Introduced navigation

--- Krila/ClientApp/src/components/Home/Slider/GenderSliderItem.js
+++ Krila/ClientApp/src/components/Home/Slider/GenderSliderItem.js
@@ -1,6 +1,7 @@
+import { Link } from 'react-router-dom';
+
 const GenderSliderItem = () => {
   return (
-
     <div className="item">
       <img src="images/slider-1.jpg" alt="slide-1" className="img-responsive" />
       <div className="slider-desc">
@@ -10,14 +11,14 @@ const GenderSliderItem = () => {
               <div className="slide-offers-left">
                 <div className="slide-offers-title"><span>За</span><br />Него</div>
                 <p>Ексклузивни предложения <br />и нови колекции</p>
-                <a href="#" className="btn btn-blue">Пазарувай </a>
+                <Link to="/men" className="btn btn-blue">Пазарувай </Link>
               </div>
             </div>
             <div className="col-md-6">
               <div className="slide-offers-right">
                 <div className="slide-offers-title"><span>За</span><br />НЕЯ</div>
                 <p>Ексклузивни предложения <br />и нови колекции</p>
-                <a href="" className="btn btn-magenta">Пазарувай </a>
+                <Link to="/women" className="btn btn-blue">Пазарувай </Link>
               </div>
             </div>
           </div>
--- Krila/ClientApp/src/components/NavMenu.js
+++ Krila/ClientApp/src/components/NavMenu.js
@@ -1,49 +1,67 @@
-import React, { Component } from 'react';
-import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import './NavMenu.css';
 
-export class NavMenu extends Component {
-  static displayName = NavMenu.name;
+const NavMenu = () => {
+  return (
+    <div id="header">
+      <div id="believe-nav">
+        <div class="container">
+          <div class="min-marg">
+            <nav class="navbar navbar-default">
+              <div class="container-fluid">
+                <div class="navbar-header">
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                  <Link to="/" class="navbar-brand">KRILA</Link>
+                </div>
 
-  constructor (props) {
-    super(props);
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                  <ul class="nav navbar-nav">
+                    <li class="active"><Link to="/">Home <span class="sr-only">(current)</span></Link></li>
+                    <li><Link to="/men">Мъже</Link></li>
+                    <li><Link to="/women">Жени</Link></li>
+                    <li><Link to="/kids">Деца</Link></li>
+                    <li><Link to="/contacts">Контакти</Link></li>
+                    <li><Link to="/about">За нас</Link></li>
+                  </ul>
 
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
+                  <ul class="nav navbar-nav navbar-right">
+                    <li class="menu-search-form">
+                      <Link to="#" id="open-srch-form"><img src="images/srch.png" alt="srch" /></Link>
+                    </li>
+                    <li><Link to="#"><img src="images/pav.png" alt="pav" /><span>2</span></Link></li>
+                    <li><Link to="#"><img src="images/bag.png" alt="bag" /><span>2</span></Link></li>
+                    <li id="open-srch-form-mod">
+                      <div>
+                        <form class="side-search">
+                          <div class="input-group">
+                            <input type="text" class="form-control search-wid" placeholder="Search Here" aria-describedby="basic-addon1" />
+                            <Link to="" class="input-group-addon btn-side-serach" id="basic-addon1"><i class="fa fa-search"></i></Link>
+                          </div>
+                        </form>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
 
-  toggleNavbar () {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
-
-  render () {
-    return (
-      <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
-          <Container>
-            <NavbarBrand tag={Link} to="/">Krila</NavbarBrand>
-            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-            <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-              <ul className="navbar-nav flex-grow">
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
-                </NavItem>
-              </ul>
-            </Collapse>
-          </Container>
-        </Navbar>
-      </header>
-    );
-  }
+            </nav>
+          </div>
+          <div class="srch-form">
+            <form class="side-search">
+              <div class="input-group">
+                <input type="text" class="form-control search-wid" placeholder="Search Here" aria-describedby="basic-addon2" />
+                <Link to="" class="input-group-addon btn-side-serach" id="basic-addon2"><i class="fa fa-search"></i></Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default NavMenu;
