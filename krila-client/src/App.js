0adfb390ce39bdec64035fdef8a91f3627b07aa0
Ati Vassileva <ativassileva@gmail.com>
2022-02-27 20:13:18 +0200

    Refactored client side

--- krila-client/src/App.js
+++ krila-client/src/App.js
@@ -1,25 +1,21 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
-import  Home  from './components/Home/Home';
-import Men from './components/Men/Men';
-import { FetchData } from './components/FetchData';
-
-import './custom.css'
+import { Route, Switch } from 'react-router-dom';
+import Home from './components/Home/Home';
 import NavMenu from './components/NavMenu';
+import Men from './components/Men/Men';
 import Footer from './components/Footer';
 
-export default class App extends Component {
-  static displayName = App.name;
+function App() {
 
-  render () {
-    return (
-      <>
-      <NavMenu/>
-        <Route exact path='/' component={Home} />
+  return (
+    <div>
+      <NavMenu />
+      <Switch>
+        <Route path="/" exact component={Home} />
         <Route path='/men' component={Men}/>
-        <Route path='/fetch-data' component={FetchData} />
-        <Footer/>
-      </>
-    );
-  }
+      </Switch>
+      <Footer/>
+    </div>
+  );
 }
+
+export default App;
--- krila-client/src/components/Footer.js
+++ krila-client/src/components/Footer.js
@@ -1,28 +1,28 @@
 const Footer = () => {
     return (
         <div id="footer">
-            <div class="footer-widget">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-3">
-                            <div class="text-widget">
-                                <div class="wid-title">KRILA</div>
+            <div className="footer-widget">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-3">
+                            <div className="text-widget">
+                                <div className="wid-title">KRILA</div>
                                 <p>
                                     Крила е онлайн магазин с оригинални и актуални за времето стоки на достъпна цена. <a href="">Пазарувай сега</a>
                                 </p>
-                                <ul class="ft-soc clearfix">
-                                    <li><a href=""><i class="fa fa-facebook-square"></i></a></li>
-                                    <li><a href=""><i class="fa fa-twitter"></i></a></li>
-                                    <li><a href=""><i class="fa fa-google-plus-square"></i></a></li>
-                                    <li><a href=""><i class="fa fa-instagram"></i></a></li>
-                                    <li><a href=""><i class="fa fa-pinterest"></i></a></li>
+                                <ul className="ft-soc clearfix">
+                                    <li><a href=""><i className="fa fa-facebook-square"></i></a></li>
+                                    <li><a href=""><i className="fa fa-twitter"></i></a></li>
+                                    <li><a href=""><i className="fa fa-google-plus-square"></i></a></li>
+                                    <li><a href=""><i className="fa fa-instagram"></i></a></li>
+                                    <li><a href=""><i className="fa fa-pinterest"></i></a></li>
                                 </ul>
-                                <div class="clearfix"></div>
+                                <div className="clearfix"></div>
                             </div>
                         </div>
-                        <div class="col-md-2">
-                            <div class="quick-links">
-                                <div class="wid-title">Бързи линкове</div>
+                        <div className="col-md-2">
+                            <div className="quick-links">
+                                <div className="wid-title">Бързи линкове</div>
                                 <ul>
                                     <li><a href="/men">Мъже</a></li>
                                     <li><a href="/women">Жени</a></li>
@@ -33,9 +33,9 @@ const Footer = () => {
                                 </ul>
                             </div>
                         </div>
-                        <div class="col-md-2">
-                            <div class="term">
-                                <div class="wid-title">&nbsp;</div>
+                        <div className="col-md-2">
+                            <div className="term">
+                                <div className="wid-title">&nbsp;</div>
                                 <p>
                                     <a href="#">Доставка</a><br />
                                     <a href="#">Условия за ползване</a><br />
@@ -44,25 +44,25 @@ const Footer = () => {
                                 </p>
                             </div>
                         </div>
-                        <div class="col-md-3">
-                            <div class="subscribe">
-                                <div class="wid-title">Бюлетин</div>
+                        <div className="col-md-3">
+                            <div className="subscribe">
+                                <div className="wid-title">Бюлетин</div>
                                 <p>
                                     Абонирайте се за нашия бюлетин, за да не пропускате нови оферти.
                                 </p>
                                 <form>
-                                    <div class="form-group">
-                                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Вашият имейл" />
+                                    <div className="form-group">
+                                        <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Вашият имейл" />
                                     </div>
-                                    <button type="submit" class="btn btn-default">Абонирай се</button>
+                                    <button type="submit" className="btn btn-default">Абонирай се</button>
                                 </form>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="footer-text">
-                <div class="container">
+            <div className="footer-text">
+                <div className="container">
                     <p>Copyright 2018. Designed and Developed by <a href="https://bootstrapmart.com/">BootstrapMart </a> &amp; Distributed by <a href="https://themewagon.com/">ThemeWagon</a></p>
                 </div>
             </div>
